39e004b15f36099be74f0a794dae71f7238a3ef2
Andrew Cabrera <andrewcabrera@Mac.attlocal.net>
2025-03-01 12:22:04 -0800

    feature(slide): added home slide

---
+++ src/app/component/slides/home.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+const HomeSlide = () => {
+  return (
+    <div className="relative mx-auto px-20 max-w-[1000px] h-screen">
+      <div className="absolute bottom-[30%]">
+        <h1>Hello! I am Andrew and I am a Full Stack Developer!</h1>
+
+        {/* TODO: add a subtext */}
+        <div></div>
+
+        {/* TODO: add desciption */}
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, iste
+          tempore inventore facere, perspiciatis ipsa natus dignissimos iusto
+          sed quas expedita ad deserunt voluptatem fugit eum quam dolorum
+          repudiandae error? Autem, facere incidunt! Debitis suscipit animi
+          aspernatur vero quae non minima autem impedit quasi maxime, vel
+          repellendus necessitatibus neque rerum? Harum impedit velit et est
+          totam adipisci, iure expedita itaque. Fugit quod enim delectus! Culpa
+          molestias iure nisi itaque officiis voluptas, atque animi facere
+          praesentium sequi nihil doloribus vel fugit nobis perspiciatis nostrum
+          totam soluta porro dolorum laudantium, consequatur pariatur. Inventore
+          repellendus incidunt minus et eos magnam quas ipsam, culpa voluptas
+          dignissimos assumenda est numquam id, consectetur eveniet quia
+          architecto totam ullam! Commodi iusto totam architecto sequi nulla
+          voluptatem inventore.
+        </p>
+        {/* Contact Button */}
+        <Link
+          href={"mailto:andrewdcabrera@gmail.com"}
+          className="rounded-lg border w-fit h-fit px-4 py-1 hover:opacity-75"
+        >
+          Email Me
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSlide;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import EmailLink from "./component/email";
 import Header from "./component/header";
+import HomeSlide from "./component/slides/home";
 import SocialLinks from "./component/social-links";
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen">
+    <div className="text-black relative min-h-screen w-full">
       <Header />
+      {/* slides */}
+      <HomeSlide />
+      {/* "Footer" */}
       <SocialLinks />
       <EmailLink />
     </div>
